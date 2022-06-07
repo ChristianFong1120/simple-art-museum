dbdccc07abe3fbf19fcde224c784d81e6d52b741
ChristianFong1120 <74108356+ChristianFong1120@users.noreply.github.com>
2022-06-07 08:36:19 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -255,7 +255,7 @@ function drawMuseumWindow( view=museumHall) {
 
 
 		textSize(15);
-		text(poem, .35 *width, .2* height, 300, 200);
+		text(poem, .35 *width, .2* height, 200, 200);
 		box
 
 		button = createImg("img/blank.png");
